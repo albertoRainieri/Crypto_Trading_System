10a6fffd108101f123ca341901d1ab74a5d49606
albertoRainieri <alberto.rainieri.lavoro@gmail.com>
2023-03-19 11:49:31 +0100

    some fixes on tracker

--- tracker/scripts/main.py
+++ tracker/scripts/main.py
@@ -5,12 +5,17 @@ sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from database.DatabaseConnection import DatabaseConnection
 from app.Controller.LoggingController import LoggingController
 from app.Controller.TrackerController import TrackerController
+from datetime import datetime
 
 def main(db, logger):
     logger.info('Hello there')
     while True:
-        #TrackerController.getData(db_trades=db, logger=logger)
-        sleep(60)
+        now=datetime.now()
+        second = now.second
+
+        if second == 0:
+            TrackerController.getData(db_trades=db, logger=logger)
+        sleep(0.9)
 
     
 
