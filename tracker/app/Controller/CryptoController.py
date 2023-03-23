b25634785021907dea5845d82e082faf9d24fbab
albertoRainieri <alberto.rainieri.lavoro@gmail.com>
2023-03-23 11:15:05 +0100

    added new feature to benchmark

--- tracker/app/Controller/CryptoController.py
+++ tracker/app/Controller/CryptoController.py
@@ -17,6 +17,7 @@ class CryptoController:
 
     def __init__(self) -> None:
         self.db = DatabaseConnection()
+        self.production = bool(int(os.getenv('PRODUCTION')))
 
     def getHeader():
         header= {"Content Type": "application/json"}
@@ -50,6 +51,8 @@ class CryptoController:
         # f = open ('/backend/json/coin-list.json', "r")
         # data = json.loads(f.read())
 
+        db_benchmark = self.get_db(DATABASE_BENCHMARK)
+        list_coins_benchmark = db_benchmark.list_collection_names()
         list_instruments = CryptoController.getAllinstruments()
 
         data = {'coin_list': list_instruments}
@@ -64,7 +67,12 @@ class CryptoController:
         pairs_traded_nrd = [] #pairs traded non-redundant
 
         # get info on tickers from Crypto.COM API
+        i = 0
+        len_coin_list = len(data['coin_list'])
         for coin in data['coin_list']:
+            i += 1
+            if not self.production:
+                print(str(i)+ '/' + str(len_coin_list))
             coin_ticker = CryptoController.getTicker(coin)
             doc_= coin_ticker['result']['data'][0]
             doc_['vv'] = float(doc_['vv'])
@@ -87,6 +95,70 @@ class CryptoController:
                 coins_traded.append(coin_0)
                 pairs_traded_nrd.append(coin)
                 most_traded_coin_list['most_traded_coin_list'].append(coin)
+        
+
+        for coin in list_coins_benchmark:
+            # update benchmark db --> (field: best trades)
+            cursor_benchmark = list(db_benchmark[coin].find())
+            id_benchmark = cursor_benchmark[0]['_id']
+
+            if coin in pairs_traded_nrd[:NUMBER_COINS_TO_TRADE*SLICES+COINS_PRIORITY]:
+
+                if 'Best_Trades' in cursor_benchmark[0]:
+                    trade_score = cursor_benchmark[0]['Best_Trades']
+                    trade_score_split = trade_score.split('/')
+                    new_score = int(trade_score_split[0]) + 1
+                    new_total = int(trade_score_split[-1]) + 1
+                    new_trade_score = str(new_score) + '/' + str(new_total)
+
+                    last_30_trades = cursor_benchmark[0]['Last_30_Trades']
+
+                    # if len of this list is greater than 30 (1 month) I delete the oldest one (first element of the list)
+                    if len(last_30_trades['list_last_30_trades']) == 30:
+                        last_30_trades['list_last_30_trades'].append(1)
+                        last_30_trades['list_last_30_trades'] = last_30_trades['list_last_30_trades'][1:]
+                    else:
+                        last_30_trades['list_last_30_trades'].append(1)
+                    last_30_trades['score_last_30_trades'] = sum(last_30_trades['list_last_30_trades']) / len(last_30_trades['list_last_30_trades'])
+
+                    db_benchmark[coin].update_one({"_id": id_benchmark}, {"$set": {"Best_Trades": new_trade_score, 'Last_30_Trades': last_30_trades}})
+                else:
+                    new_score = 1
+                    new_total = 1
+                    new_trade_score = str(new_score) + '/' + str(new_total)
+                    last_30_trades = {'list_last_30_trades': [1], 'score_last_30_trades': 1}
+
+                    db_benchmark[coin].update_one({"_id": id_benchmark}, {"$set": {"Best_Trades": new_trade_score, 'Last_30_Trades': last_30_trades}})
+            else:
+                if 'Best_Trades' in cursor_benchmark[0]:
+                    trade_score = cursor_benchmark[0]['Best_Trades']
+                    trade_score_split = trade_score.split('/')
+                    score = trade_score_split[0]
+                    new_total = int(trade_score_split[-1]) + 1
+                    new_trade_score = score + '/' + str(new_total)
+
+                    last_30_trades = cursor_benchmark[0]['Last_30_Trades']
+
+                    # if len of this list is greater than 30 (1 month) I delete the oldest one (first element of the list)
+                    if len(last_30_trades['list_last_30_trades']) == 30:
+                        last_30_trades['list_last_30_trades'].append(0)
+                        last_30_trades['list_last_30_trades'] = last_30_trades['list_last_30_trades'][1:]
+                    else:
+                        last_30_trades['list_last_30_trades'].append(0)
+                    last_30_trades['score_last_30_trades'] = sum(last_30_trades['list_last_30_trades']) / len(last_30_trades['list_last_30_trades'])
+
+                    db_benchmark[coin].update_one({"_id": id_benchmark}, {"$set": {"Best_Trades": new_trade_score, 'Last_30_Trades': last_30_trades}})
+                
+                else:
+                    new_score = 0
+                    new_total = 1
+                    new_trade_score = str(new_score) + '/' + str(new_total)
+                    last_30_trades = {'list_last_30_trades': [0], 'score_last_30_trades': 0}
+
+                    db_benchmark[coin].update_one({"_id": id_benchmark}, {"$set": {"Best_Trades": new_trade_score, 'Last_30_Trades': last_30_trades}})
+
+
+            
         
         all_info_coins_daily_new = {}
 
@@ -136,13 +208,12 @@ class CryptoController:
 
         body=CryptoController.getBody(method=method, nonce=nonce, id=id)
         header=CryptoController.getHeader()
-        print(body)
+        #print(body)
 
         response=requests.get(url=REST_API_ENDPOINT + method, data=body, headers=header)
         return json.loads(response.text)
     
     @staticmethod
-    @timer_func
     def getTicker(instrument_name='BTC_USD'):
         header=CryptoController.getHeader()
 
