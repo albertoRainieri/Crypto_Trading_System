fb3e8d76d5a26db61dcb1c00d54e549b61126755
albertoRainieri <alberto.rainieri.lavoro@gmail.com>
2023-05-23 12:06:03 +0200

    debugging analysiscontroller

--- backend/app/Controller/AnalysisController.py
+++ backend/app/Controller/AnalysisController.py
@@ -81,6 +81,9 @@ class AnalysisController:
                             volume_last_7days_std_list.append(volume_series[date][1])
 
                     if len(volume_last_7days_mean_list) > 0:
+                        if coin == 'LOOMUSDT':
+                            print(volume_last_7days_mean_list)
+                            
                         vol_mean_7days = np.mean(volume_last_7days_mean_list)
                         vol_std_7days = np.mean(volume_last_7days_std_list)
 
