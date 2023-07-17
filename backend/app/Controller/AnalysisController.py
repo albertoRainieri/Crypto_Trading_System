f00ab3df2e536aaebce9ea8117054ab5d0eb5a5e
albertoRainieri <alberto.rainieri.lavoro@gmail.com>
2023-07-17 22:58:27 +0200

    analysiscontroller check_future

--- backend/app/Controller/AnalysisController.py
+++ backend/app/Controller/AnalysisController.py
@@ -168,7 +168,7 @@ class AnalysisController:
                         timeseries_start = datetime.fromisoformat(event['event']).replace(second=0, microsecond=0)
                         # datetime_start is the beginning start from which retrieving the observation
                         datetime_start = timeseries_start - timedelta(minutes=check_past)
-                        datetime_end = timeseries_start + timedelta(minutes=timeframe)
+                        datetime_end = timeseries_start + timedelta(minutes=timeframe) + timedelta(minutes=check_past)
                     
                     # let's get the iso format timestamps for querying mongodb
                     timestamp_start = datetime_start.isoformat()
