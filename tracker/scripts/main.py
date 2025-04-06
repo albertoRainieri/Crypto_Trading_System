89b59149f9869f39b22abdb5936e4c4e63ab3d8b
alberto.rainieri <alberto.rainieri@consulenti.mediolanum.it>
2025-04-06 20:25:14 +0200

    fix

--- tracker/scripts/main.py
+++ tracker/scripts/main.py
@@ -1,11 +1,17 @@
-
 from time import time, sleep
 import sys, os
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
-from app.Helpers.Helpers import round_, get_best_coins, get_volume_standings, get_coins_list
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+from app.Helpers.Helpers import (
+    round_,
+    get_best_coins,
+    get_volume_standings,
+    get_coins_list,
+)
 from database.DatabaseConnection import DatabaseConnection
 from app.Controller.LoggingController import LoggingController
 from app.Controller.TrackerController import TrackerController
+from app.Controller.BinanceController import BinanceController
 from datetime import datetime, timedelta
 import asyncio
 from constants.constants import *
@@ -14,10 +20,21 @@ import json
 SECOND_START_TRACKING = 2
 FIRST_RUN = True
 
-def main(db_trades, db_tracker, db_benchmark, db_logger, db_volume_standings, logger, FIRST_RUN, SECOND_START_TRACKING, coins_list):
-    '''
+
+def main(
+    db_trades,
+    db_tracker,
+    db_benchmark,
+    db_logger,
+    db_volume_standings,
+    logger,
+    FIRST_RUN,
+    SECOND_START_TRACKING,
+    coins_list,
+):
+    """
     This function tracks the statistics of the most traded pairs each minute
-    '''
+    """
 
     volume_standings = get_volume_standings(db_volume_standings)
     best_x_coins = get_best_coins(volume_standings)
@@ -27,30 +44,38 @@ def main(db_trades, db_tracker, db_benchmark, db_logger, db_volume_standings, lo
         now = datetime.now()
         if now.second == SECOND_START_TRACKING:
             t1 = time()
-            TrackerController.start_tracking(best_x_coins, coins_list, volume_standings=volume_standings, db_trades=db_trades, db_tracker=db_tracker,
-                                              db_benchmark=db_benchmark, logger=logger, db_logger=db_logger)
+            TrackerController.start_tracking(
+                best_x_coins,
+                coins_list,
+                volume_standings=volume_standings,
+                db_trades=db_trades,
+                db_tracker=db_tracker,
+                db_benchmark=db_benchmark,
+                logger=logger,
+                db_logger=db_logger,
+            )
             if now.hour == 23 and now.minute == 59:
                 coins_list = get_coins_list()
             if now.hour == 0 and now.minute == 0:
                 t2 = time()
-                timespent = round_(t2-t1,3)
-                logger.info(f'start_tracking executed in {timespent}s')
+                timespent = round_(t2 - t1, 3)
+                logger.info(f"start_tracking executed in {timespent}s")
                 volume_standings = get_volume_standings(db_volume_standings)
                 best_x_coins = get_best_coins(volume_standings)
-    
+
             # sleep until next run of tracking
             FIRST_RUN = False
-            time_delta = (datetime.now() + timedelta(minutes=1)).replace(second=SECOND_START_TRACKING, microsecond=0) - datetime.now()
+            time_delta = (datetime.now() + timedelta(minutes=1)).replace(
+                second=SECOND_START_TRACKING, microsecond=0
+            ) - datetime.now()
             sleep_seconds = time_delta.seconds + (time_delta.microseconds / 10**6)
             sleep(sleep_seconds)
-            
 
         if FIRST_RUN:
             sleep(0.5)
 
-    
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     client = DatabaseConnection()
     logger = LoggingController.start_logging()
     db_trades = client.get_db(database=DATABASE_MARKET)
@@ -62,9 +87,18 @@ if __name__ == '__main__':
     SECOND_START_TRACKING = 3
     FIRST_RUN = True
 
-
     sleep(2)
-    logger.info('Main Script Started')
+    logger.info("Main Script Started")
+    asyncio.run(BinanceController.get_exchange_info(logger=logger))
     coins_list = get_coins_list()
-    main(db_trades=db_trades, db_tracker=db_tracker, db_benchmark=db_benchmark, db_logger=db_logger,
-          db_volume_standings=db_volume_standings, logger=logger, FIRST_RUN=FIRST_RUN, SECOND_START_TRACKING=SECOND_START_TRACKING, coins_list=coins_list)
+    main(
+        db_trades=db_trades,
+        db_tracker=db_tracker,
+        db_benchmark=db_benchmark,
+        db_logger=db_logger,
+        db_volume_standings=db_volume_standings,
+        logger=logger,
+        FIRST_RUN=FIRST_RUN,
+        SECOND_START_TRACKING=SECOND_START_TRACKING,
+        coins_list=coins_list,
+    )
--- tracker/scripts/side-script.py
+++ tracker/scripts/side-script.py
@@ -1,6 +1,6 @@
-
 import sys, os
-sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
 
 from time import sleep
 from app.Controller.LoggingController import LoggingController
@@ -15,49 +15,52 @@ import asyncio
 
 
 def main(client, logger, db_logger, user_configuration):
-    logger.info('Side Script Started')
+    logger.info("Side Script Started")
     while True:
-        
-        now=datetime.now()
+
+        now = datetime.now()
         minute = now.minute
         second = now.second
         hour = now.hour
 
         if minute == 59 and second == 15 and hour == 23:
-        #if second == 10:
+            # if second == 10:
             TradingController.checkPerformance(client, logger, user_configuration)
-            logger.info("Performance has been updated from 'TradingController.checkPerformance'")
+            logger.info(
+                "Performance has been updated from 'TradingController.checkPerformance'"
+            )
             sleep(0.2)
 
         if minute == 59 and second == 18 and hour == 23:
             asyncio.run(BinanceController.get_exchange_info(logger=logger))
-            logger.info("list of instruments updated from 'BinanceController.get_exchange_info'")
+            logger.info(
+                "list of instruments updated from 'BinanceController.get_exchange_info'"
+            )
 
         if minute == 59 and second == 30 and hour == 23:
             Benchmark.computeVolumeAverage(client=client)
-            logger.info("volumes have been updated from 'Benchmark.computeVolumeAverage'")
-        
+            logger.info(
+                "volumes have been updated from 'Benchmark.computeVolumeAverage'"
+            )
+
         # if second == 55:
         #     #logger.info('Updating Balance Account')
         #     f = open ('/tracker/user_configuration/userconfiguration.json', "r")
         #     user_configuration = json.loads(f.read())
         #     TradingController.get_balance_account(client, logger, user_configuration)
         #     sleep(0.2)
-        
+
         sleep(0.8)
 
-    
 
-    
+if __name__ == "__main__":
 
-if __name__ == '__main__':
-    
     client = DatabaseConnection()
     db_logger = client.get_db(DATABASE_API_ERROR)
 
     logger = LoggingController.start_logging()
 
-    f = open ('/tracker/user_configuration/userconfiguration.json', "r")
+    f = open("/tracker/user_configuration/userconfiguration.json", "r")
     user_configuration = json.loads(f.read())
 
     sleep(2)
@@ -65,11 +68,14 @@ if __name__ == '__main__':
     TradingController.restart_order_book_polling(logger)
 
     for user in user_configuration:
-        if user_configuration[user]['trading_live']:
-            logger.info(f'TRADING_LIVE is enabled for {user}')
+        if user_configuration[user]["trading_live"]:
+            logger.info(f"TRADING_LIVE is enabled for {user}")
         else:
-            logger.info(f'TRADING_LIVE is NOT enabled for {user}')
+            logger.info(f"TRADING_LIVE is NOT enabled for {user}")
 
-    asyncio.run(BinanceController.get_exchange_info(logger=None))
-    
-    main(client=client, logger=logger, db_logger=db_logger, user_configuration=user_configuration)
+    main(
+        client=client,
+        logger=logger,
+        db_logger=db_logger,
+        user_configuration=user_configuration,
+    )
